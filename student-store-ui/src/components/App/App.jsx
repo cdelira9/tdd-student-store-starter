1702acbbf164ddf3cc93d9f15184610d48512bdc
cdelira9 <cdelira@ucsd.edu>
2022-06-21 15:23:31 -0700

    step 3

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -3,17 +3,21 @@ import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
 import "./App.css"
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 export default function App() {
   return (
     <div className="app">
+      
       <BrowserRouter>
         <main>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/products/:productId" element={<ProductDetail/>} />
+          <Route path="*" element={<NotFound/>}/>
+          
+        </Routes>
           {/* YOUR CODE HERE! */}
-          <Navbar />
-          <Sidebar />
-          <Home />
         </main>
       </BrowserRouter>
     </div>
