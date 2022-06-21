69720908b45356ddda7eb9f5c574dc77390ebe4f
cdelira9 <cdelira@ucsd.edu>
2022-06-21 15:23:38 -0700

    step 3

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -1,10 +1,14 @@
 import * as React from "react"
+import Navbar from "../Navbar/Navbar"
+import Sidebar from "../Sidebar/Sidebar"
 import "./Home.css"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 export default function Home() {
   return (
     <div className="home">
       <p>Home</p>
+      
     </div>
   )
 }
