c6be8b9ac36b4711063797d46e228a2836d5f2a9
cdelira9 <cdelira@ucsd.edu>
2022-06-21 16:34:02 -0700

    router with three routes

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,23 +1,36 @@
 import * as React from "react"
 import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
+import NotFound from "../NotFound/NotFound"
+import ProductDetail from "../ProductDetail/ProductDetail"
 import Home from "../Home/Home"
+
+
 import "./App.css"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 export default function App() {
+  var products = [];
+  
   return (
     <div className="app">
-      
+
+
       <BrowserRouter>
         <main>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/products/:productId" element={<ProductDetail/>} />
           <Route path="*" element={<NotFound/>}/>
+          <Route path="navbar" element={<Navbar/>}/>
+          <Route path="sidebar" element={<Sidebar/>}/>
           
         </Routes>
           {/* YOUR CODE HERE! */}
+          <Navbar />
+          <Sidebar />
+          <Home />
+
         </main>
       </BrowserRouter>
     </div>
---
+++ student-store-ui/src/components/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import "./ProductDetail.css"
+
+export default function ProductDetail() {
+  return (
+    <div className="productdetail">
+      <p>Product Detail</p>
+    </div>
+  )
+}
