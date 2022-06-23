dc0edb957f82979bb5b5f1ed1e0813eddc1dc4ef
cdelira9 <cdelira@ucsd.edu>
2022-06-23 16:20:31 -0700

    fetch data

--- student-store-ui/src/components/ProductGrid/ProductGrid.jsx
+++ student-store-ui/src/components/ProductGrid/ProductGrid.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import "./ProductGrid.css";
 
-export default function ProductGrid() {
+export default function ProductGrid(products) {
   return (
     <div className="product-grid">
       <p>Product Grid</p>
